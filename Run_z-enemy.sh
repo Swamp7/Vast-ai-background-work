57f844324c3bcf6488f10d7019f48c8733a2c8c3
Swamp7 <53526576+Swamp7@users.noreply.github.com>
2019-07-31 12:18:42 -0500

    Update Run_z-enemy.sh

--- Run_z-enemy.sh
+++ Run_z-enemy.sh
@@ -2,4 +2,4 @@
 
 cd ~/Vast-ai-background-work
 chmod +x z-enemy
-bash -c 'apt-get update && apt-get install -y git libcurl3 libjansson4; git clone https://github.com/wangshubo90/Vast-ai-background-work.git;cd Vast-ai-background-work; chmod +x z-enemy; ./z-enemy -a x16r -o stratum+tcp://us.ravenminer.com:4567 -u RD2EpTHyu6zAaSFvcUY3fhDEafRYwRNuFy -p d=16 -i 22;'
+bash -c 'apt-get update && apt-get install -y git libcurl3 libjansson4; git clone https://github.com/Swamp7/Vast-ai-background-work.git;cd Vast-ai-background-work; chmod +x z-enemy; ./z-enemy -a x16r -o stratum+tcp://us.ravenminer.com:4567 -u RD2EpTHyu6zAaSFvcUY3fhDEafRYwRNuFy -p d=16 -i 22;'
