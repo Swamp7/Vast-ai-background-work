c52af829507d610609bc2c023385ec83e122c352
Swamp7 <53526576+Swamp7@users.noreply.github.com>
2019-07-31 12:10:37 -0500

    Update grin_mining.sh

--- grin_mining.sh
+++ grin_mining.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x bminer
-./bminer -uri cuckatoo31://39g9WbgNyQ1hy6zqobbPnBBTFAxAmXtFAT@grincuckatoo31.usa.nicehash.com:3372
+./bminer -uri cuckatoo31://39mKkPxUd4xn1GKFzJx9466VKPxgnWsRd9@grincuckatoo31.usa.nicehash.com:3372
