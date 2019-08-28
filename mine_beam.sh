160a7228f4d9a0e7265768c7db780d71a12445d4
Swamp7 <53526576+Swamp7@users.noreply.github.com>
2019-08-28 17:19:00 -0500

    Update mine_beam.sh

--- mine_beam.sh
+++ mine_beam.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 
-
-
-
 chmod +x miner
-
 ./miner --algo beamhash --server stratum+tcp://beamv2.usa-new.nicehash.com --port 3378 --ssl 1 --user 3KCMYbCf6qyRqpKDVt1izBaHnH7371wpNh.rig3
